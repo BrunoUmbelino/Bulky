faf53b2d3ac8338159ed9fc9989e13e9bfd24548
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-07-18 20:29:46 -0300

    Adicionando funcionalidade e tela de Lista de Categorias e Nova Categoria

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category newCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Categories.Add(newCategory);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ BulkyWeb/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id = 1, DisplayOrder = 1, Name = "Ficção Científica" },
+                new Category() { Id = 2, DisplayOrder = 2, Name = "Terror" },
+                new Category() { Id = 3, DisplayOrder = 3, Name = "Ação e aventura" }
+                );
+        }
+    }
+}

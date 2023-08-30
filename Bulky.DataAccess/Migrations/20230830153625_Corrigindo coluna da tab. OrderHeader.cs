5250e7ca46d3e2c64ea8279e5c7ceb3d70b9ca1f
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-08-30 12:39:14 -0300

    Corrigindo prop/coluna em OrderHeader

---
+++ Bulky.DataAccess/Migrations/20230830153625_Corrigindo coluna da tab. OrderHeader.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrigindocolunadatabOrderHeader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "OrderHeader",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "OrderHeader",
+                newName: "MyProperty");
+        }
+    }
+}
--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -30,6 +30,6 @@ namespace Bulky.Models
         [Required] public string City { get; set; } = string.Empty;
         [Required] public string State { get; set; } = string.Empty;
         [Required] public string PostalCode { get; set; } = string.Empty;
-        [Required] public string MyProperty { get; set; } = string.Empty;
+        [Required] public string Name { get; set; } = string.Empty;
     }
 }
